6d9aa0513b4f373e0b26949d0b12604f626b29c1
Dipali <dipalikharat2798@gmail.com>
2022-01-20 15:11:55 +0530

    initial commit

---
+++ app/src/main/java/com/androdevdk/scanbarcodezxingapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.androdevdk.scanbarcodezxingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.androdevdk.scanbarcodezxingapp.databinding.ActivityMainBinding
+import com.google.zxing.client.android.Intents
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val barcodeLauncher = registerForActivityResult(ScanContract()) {
+        if (it.getContents() == null) {
+            val originalIntent: Intent = it.getOriginalIntent()
+            if (originalIntent == null) {
+                Log.d("MainActivity", "Cancelled scan")
+                Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
+            } else if (originalIntent.hasExtra(Intents.Scan.MISSING_CAMERA_PERMISSION)) {
+                Log.d("MainActivity", "Cancelled scan due to missing camera permission")
+                Toast.makeText(
+                    this@MainActivity,
+                    "Cancelled due to missing camera permission",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        } else {
+            Log.d("MainActivity", "Scanned")
+            binding.textContent.setText(it.getContents())
+            binding.textFormat.setText(it.getFormatName())
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // adding listener to the button
+        binding.scanBtn.setOnClickListener(listener)
+    }
+
+    val listener = View.OnClickListener { view ->
+        when (view.getId()) {
+            R.id.scanBtn -> {
+                scan()
+            }
+        }
+    }
+
+    private fun scan() {
+        val options = ScanOptions()
+        options.setPrompt("Scan a barcode or QR Code") //message
+        options.setCameraId(0) //use specific camera of device
+        options.setOrientationLocked(false);
+        options.setBarcodeImageEnabled(true)
+        barcodeLauncher.launch(options)
+    }
+
+}
